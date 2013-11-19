31a5d023dcb4ee9670ec530f76f1b0ed255de1c1
shu0115 <s.matsumoto0115@gmail.com>
2013-11-19 14:30:03 +0900

    アップデート

--- lib/markdown-scaffold.rb
+++ lib/markdown-scaffold.rb
@@ -14,23 +14,22 @@ module Markdown
       # Generateコマンド実行処理
       def generate_scaffold
         # stylesheets/pygments.css
-        copy_file( "templates/pygments.css", "app/assets/stylesheets/pygments.css" )
+        copy_file("templates/pygments.css", "app/assets/stylesheets/pygments.css")
 
         # models/html_with_pygments.rb
-        copy_file( "templates/html_with_pygments.rb", "app/models/html_with_pygments.rb" )
+        copy_file("templates/html_with_pygments.rb", "app/models/html_with_pygments.rb")
 
         # controllers/application_controller.rb
         content = "\n"
         content += "  # Markdown変換表示\n"
-        content += "  def show_markdown( text )\n"
-        content += "    html_render = HtmlWithPygments.new( hard_wrap: true, filter_html: true )\n"
-        content += "    markdown    = Redcarpet::Markdown.new( html_render, autolink: true, fenced_code_blocks: true, space_after_headers: true )\n"
+        content += "  def show_markdown(text)\n"
+        content += "    html_render = HtmlWithPygments.new(hard_wrap: true, filter_html: true)\n"
+        content += "    markdown    = Redcarpet::Markdown.new(html_render, autolink: true, fenced_code_blocks: true, space_after_headers: true)\n"
         content += "\n"
-        content += "    return markdown.render( text.to_s ) rescue text\n"
+        content += "    return markdown.render(text.to_s) rescue text\n"
         content += "  end\n"
         content += "  helper_method :show_markdown\n"
-
-        insert_into_file( "app/controllers/application_controller.rb", content.force_encoding('ASCII-8BIT'), after: "private\n" )
+        insert_into_file("app/controllers/application_controller.rb", content.force_encoding('ASCII-8BIT'), after: "private\n")
 
         # initializers/ruby_python.rb
 #        copy_file( "templates/ruby_python.rb", "config/initializers/ruby_python.rb" )
--- lib/markdown-scaffold/templates/html_with_pygments.rb
+++ lib/markdown-scaffold/templates/html_with_pygments.rb
@@ -1,7 +1,6 @@
 class HtmlWithPygments < Redcarpet::Render::HTML
-
   # コードハイライト
-  def block_code( code, language )
-    Pygments.highlight( code, lexer: language, options: { encoding: 'utf-8' } )
+  def block_code(code, language)
+    Pygments.highlight(code, lexer: language, options: { encoding: 'utf-8' })
   end
 end
